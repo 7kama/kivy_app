0771cd394c613becd255a0619956036431634e4a
7kuma <neus0wch1ck@gmail.com>
2022-06-24 23:42:08 +0900

    Set up the project again

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from app.view.main import MyBusinessApp
+# from app.view.main import MyBusinessApp
 
 
 def main():
-    gui_app = MyBusinessApp()
-    gui_app.run()
+    print('success')
+    # gui_app = MyBusinessApp()
+    # gui_app.run()
 
 
 if __name__ == '__main__':
